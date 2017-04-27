f542438b2e183bd63a2e11a5f105b1919edfa16e
sky <bluesky@skydembp.home>
2017-04-27 11:05:36 +0800

    你到底在哪？強尼哥...

---
+++ Missionsss/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  Missionsss
+//
+//  Created by sky on 2017/4/26.
+//  Copyright © 2017年 sky. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var img: UIImageView!
+    
+}
